4f7e5cd00ccd141ac9790eb9ed46e90a3eb09189
Jennifer <jennifershinshin@gmail.com>
2016-03-22 16:46:15 -0700

    Passed tests hello-world

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,7 +2,7 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        String versionStr = System.getProperty("java.version");
+        String versionStr = System.getProperty("java.specification.version");
         return Double.parseDouble(versionStr);
     }
 
